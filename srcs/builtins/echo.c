db48d61b6925a7ee0f500de521eefa3c6cc5a8e2
Mathis Jameau <mjameau@made-f0Dr3s3.clusters.42paris.fr>
2024-09-25 18:34:00 +0200

    echo done + export WIP

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,8 +6,57 @@
 /*   By: mjameau <mjameau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:51:35 by mjameau           #+#    #+#             */
-/*   Updated: 2024/09/24 15:52:04 by mjameau          ###   ########.fr       */
+/*   Updated: 2024/09/25 18:32:36 by mjameau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+int	check_flag_n(char *line)
+{
+	int	i;
+
+	i = 0;
+	if (line[i] && line[i] == '-')
+	{
+		++i;
+		while (line[i] && line[i] == 'n')
+			i++;
+		if (i == (int)ft_strlen(line))
+			return (1);
+	}
+	return (0);
+}
+
+void	print_echo(int count, int i, bool n_flag, char **args)
+{
+	while (args[i] && check_flag_n(args[i]))
+	{
+		++i;
+		n_flag = false;
+	}
+	while (i < count)
+	{
+		write(1, args[i], ft_strlen(args[i]));
+		if (i != count - 1)
+			write(1, " ", 1);
+		++i;
+	}
+	if (n_flag)
+		write(1, "\n", 1);
+}
+
+int	ft_echo(char **args)
+{
+	int		count;
+	int		i;
+	bool	n_flag;
+
+	count = 0;
+	while (args[count])
+		++count;
+	i = 1;
+	n_flag = true;
+	print_echo(count, i, n_flag, args);
+	return (0);
+}
--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,8 +6,61 @@
 /*   By: mjameau <mjameau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:51:39 by mjameau           #+#    #+#             */
-/*   Updated: 2024/09/24 15:52:01 by mjameau          ###   ########.fr       */
+/*   Updated: 2024/09/25 18:32:46 by mjameau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+/*
+*
+en gros je check si args, si pas d'arguments alors j'affiche tout.
+si arguments je regarde si un env->name existe deja sous le meme nom
+si il existe deja je prends juste la valeur et remplace l'ancienne
+si il existe pas je le creer et lui attribue la valeur.
+*/
+int	export(t_env *env, char *args)
+{
+	int		i;
+	char	*equal_sign;
+
+	i = 1;
+	equal_sign = ft_strchr(&args[i - 1], '=');
+	if (!args)
+		print_env(env);
+	else
+	{
+		while (env->next != NULL)
+		{
+			if (ft_strncmp(env->name, &args[i], ft_strlen(env->name)) == 0)
+			{
+				if (equal_sign)
+					env->value = ft_strdup(equal_sign + 1);
+			}
+			else
+				add_node_env(&env, &args[i]);
+		}
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	t_env *env_list = NULL;
+	char *args = "";
+
+	// Appel à la fonction export
+	export(env_list, args);
+
+	// Afficher la liste d'environnements après l'appel de export
+	printf("\nListe des variables d'environnement après export:\n");
+	t_env *temp = env_list;
+	while (temp != NULL)
+	{
+		printf("%s=%s\n", temp->name, temp->value);
+		temp = temp->next;
+	}
+
+	// Libérer la mémoire (si nécessaire)
+	return (0);
+}
