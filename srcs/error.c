534c0aff23361c6bbc187aa409867fd77e8c4d49
Paul Martins Pereira <paulmart@made-f0Br3s11.clusters.42paris.fr>
2024-09-26 15:59:34 +0200

    PUTEUX

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 13:33:24 by paulmart          #+#    #+#             */
-/*   Updated: 2024/09/25 17:15:57 by paulmart         ###   ########.fr       */
+/*   Updated: 2024/09/26 15:41:02 by paulmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,26 @@
 void	error_exit(char *str, t_global *glob)
 {
 	free_env(glob->env);
-	free_cmd(glob->cmd);
-	free_tok(&glob->token_list);
+	free_path(glob->path);
+	if (glob->cmd)
+		free_cmd(glob->cmd);
+	if (glob->token_list)
+		free_tok(&glob->token_list);
+	if (glob->line)
+		free(glob->line);
+	rl_clear_history();
 	printf("%s", str);
 	exit(1);
+}
+
+void	free_path(char **path)
+{
+	int	i;
+
+	i = -1;
+	while (path[++i])
+		free(path[i]);
+	free(path);
 }
 
 void	free_env(t_env *a)
@@ -33,8 +49,9 @@ void	free_env(t_env *a)
 	{
 		tmp = current->next;
 		free(current->name);
-		free(current->str);
 		free(current->value);
+		if (current->str != NULL)
+			free(current->str);
 		free(current);
 		current = tmp;
 	}
