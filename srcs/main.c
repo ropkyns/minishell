11e2ebcd85a6bc8e23e9ef6afb4d6a7234167ab5
Paul Martins Pereira <paulmart@made-f0Br3s11.clusters.42paris.fr>
2024-10-11 16:02:00 +0200

    ui

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mjameau <mjameau@student.42.fr>            +#+  +:+       +#+        */
+/*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 12:29:04 by mjameau           #+#    #+#             */
-/*   Updated: 2024/10/10 14:59:05 by mjameau          ###   ########.fr       */
+/*   Updated: 2024/10/11 11:11:37 by paulmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,15 @@ int	main(int argc, char **argv, char **env)
 	while (1)
 	{
 		glob->cmd = NULL;
-		handle_signal();
+		fflush(stdout);
 		glob->line = readline("minishell > ");
 		if (!(glob->line))
 			error_exit("exit", glob);
 		add_history(glob->line);
 		if (!do_list_token(&glob->token_list, glob->line))
 			return (1);
-		init_cmd(&glob->cmd, &glob->token_list, glob);
 		print_token(glob->token_list);
+		init_cmd(&glob->cmd, &glob->token_list, glob);
 		get_cmd(glob->cmd->cmd_args, &glob, &glob->env);
 		free_cmd(glob->cmd);
 	}
