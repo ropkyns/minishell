5b61baae1b20c7d4f44d548917f6ac1474b95244
Paul Martins Pereira <paulmart@made-f0Br4s12.clusters.42paris.fr>
2024-11-09 12:43:56 +0100

    norminage $

--- srcs/parsing/even_more_syntax.c
+++ srcs/parsing/even_more_syntax.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   even_more_syntax.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: palu <palu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:05:30 by paulmart          #+#    #+#             */
-/*   Updated: 2024/11/05 14:50:00 by palu             ###   ########.fr       */
+/*   Updated: 2024/11/09 12:11:54 by paulmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,44 +32,54 @@ char	*search_env(char *value, t_env *env)
 	return (ft_strdup("\0"));
 }
 
-char	*after_dollar(char *line, size_t *i, t_env *env, t_global *glob)
+char	*dollar_exit_value(char *line, size_t *i, t_global *glob)
+{
+	char	*new_line;
+
+	new_line = ft_strncpy(ft_calloc((*i + 1), sizeof(char)), line, (*i) - 1);
+	new_line = ft_strjoin(new_line, ft_itoa(glob->exit_value));
+	new_line = ft_strjoin(new_line, line + (*i) + 1);
+	free(line);
+	line = ft_strdup(new_line);
+	free(new_line);
+	return (line);
+}
+
+char	*dollar_else(char *line, size_t *i, t_env *env)
 {
 	char	*new_line;
 	char	*dollar;
 	size_t	j;
 
-	new_line = NULL;
-	if (line[(*i)] == '?')
-	{
-		new_line = ft_strncpy(ft_calloc((*i + 1), sizeof(char)), line, (*i) - 1);
-		new_line = ft_strjoin(new_line, ft_itoa(glob->exit_value));
-		new_line = ft_strjoin(new_line, line + (*i) + 1);
-		free(line);
-		line = ft_strdup(new_line);
-		free(new_line);
-	}
+	j = (*i);
+	if (ft_isdigit(line[j]))
+		while (ft_isdigit(line[j]))
+			j++;
+	else if (ft_isalpha(line[j]))
+		while (ft_isalnum(line[j]) && line[j])
+			j++;
 	else
-	{
-		j = (*i);
-		if (ft_isdigit(line[j]))
-			while (ft_isdigit(line[j]))
-				j++;
-		else if (ft_isalpha(line[j]))
-			while (ft_isalnum(line[j]) && line[j])
-				j++;
-		else
-			return (line);
-		dollar = ft_strncpy(ft_calloc(j - (*i) + 1, sizeof(char)), line + (*i), j - (*i));
-		new_line = ft_strncpy(ft_calloc((*i), sizeof(char)), line, (*i) - 1);
-		dollar = search_env(dollar, env);
-		new_line = ft_strjoin(new_line, dollar);
-		(*i) = j;
-		new_line = ft_strjoin(new_line, line + (*i));
-		free(line);
-		line = ft_strdup(new_line);
-		free(new_line);
-		free(dollar);
-	}
+		return (line);
+	dollar = ft_strncpy(ft_calloc(j - (*i) + 1, sizeof(char)),
+			line + (*i), j - (*i));
+	new_line = ft_strncpy(ft_calloc((*i), sizeof(char)), line, (*i) - 1);
+	dollar = search_env(dollar, env);
+	new_line = ft_strjoin(new_line, dollar);
+	(*i) = j;
+	new_line = ft_strjoin(new_line, line + (*i));
+	free(line);
+	line = ft_strdup(new_line);
+	free(new_line);
+	free(dollar);
+	return (line);
+}
+
+char	*after_dollar(char *line, size_t *i, t_env *env, t_global *glob)
+{
+	if (line[(*i)] == '?')
+		line = dollar_exit_value(line, i, glob);
+	else
+		line = dollar_else(line, i, env);
 	return (line);
 }
 
